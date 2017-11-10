79d6448722581fd9239b619601024fb60eb2db7f
konojunya <junya03dance@gmail.com>
2017-11-10 19:04:58 +0900

    depに変えた

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hal-iosk/hal-cinema/middleware"
-	cors "github.com/itsjamie/gin-cors"
 )
 
 func main() {
@@ -24,7 +22,6 @@ func main() {
 
 	// api routing
 	api := r.Group("/api")
-	api.Use(cors.Middleware(middleware.CorsConfig))
 
 	r.Run(":2000")
 }
